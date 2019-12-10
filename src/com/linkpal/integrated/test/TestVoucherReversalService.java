1ac23715bf2add554531ae43ef20bde55c7d8dfa
planerain <172662169@qq.com>
2019-12-10 16:32:04 +0800

    add test

---
+++ src/com/linkpal/integrated/test/TestVoucherReversalService.java
@@ -0,0 +1,31 @@
+package com.linkpal.integrated.test;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestVoucherReversalService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestVoucherReversalService.class);
+	
+	public static void main(String[] args) {
+		String jsonData = "{\r\n" + 
+				"	\"erpVoucherId\": \"4064\",\r\n" + 
+				"	\"reverseType\": \"0\",\r\n" + 
+				"	\"reverseDate\": \"2019-12-10\"\r\n" + 
+				"}";
+		JaxWsDynamicClientFactory clientFactory =JaxWsDynamicClientFactory.newInstance();
+		Client client = clientFactory.createClient("http://172.16.7.153:8080/sqgx/ws/voucherReversal?wsdl");
+		if(client!=null) {
+			LOGGER.info("链接成功");
+			System.out.println("111111111");
+			try {
+				Object[] result = client.invoke("getVoucherReversalData", jsonData);
+				LOGGER.info(result[0].toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/com/linkpal/integrated/test/TestVoucherService.java
@@ -0,0 +1,125 @@
+package com.linkpal.integrated.test;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Description:测试凭证接收接口
+ *
+ * @author lichao
+ * @date 2019年12月4日 下午4:09:29
+ *
+ */
+public class TestVoucherService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestVoucherService.class);
+	
+	public void transVoucher() {
+		String jsonData = "{\r\n" + 
+				"	\"ZWPZK_PZNM\": \"GStest201912091617\",\r\n" + 
+				"	\"ZWPZK_DWBH\": \"105\",\r\n" + 
+				"	\"ZWPZK_KJND\": \"2019\",\r\n" + 
+				"	\"ZWPZK_KJQJ\": \"11\",\r\n" + 
+				"	\"ZWPZK_PZRQ\": \"20191130\",\r\n" + 
+				"	\"ZWPZK_PZBH\": \"\",\r\n" + 
+				"	\"ZWPZK_LXBH\": \"GL现支\",\r\n" + 
+				"	\"ZWPZK_FJZS\": 0,\r\n" + 
+				"	\"ZWPZK_ZDR\": \"侯晓婷\",\r\n" + 
+				"	\"ZWPZK_TZDZS\": 0,\r\n" + 
+				"	\"ZWPZK_ZY\": \"付史秀琴应付挂账款\",\r\n" + 
+				"	\"ZWPZK_JE\": 2,\r\n" + 
+				"	\"CREATEDTIME\": \"2019-11-30\",\r\n" + 
+				"	\"ZWPZK_ZDRBH\": \"\",\r\n" + 
+				"	\"ZWPZFL\": [{\r\n" + 
+				"			\"ZWFZYS_ID\": \"\",\r\n" + 
+				"			\"ZWPZFL_ZY\": \"付史秀琴应付挂账款\",\r\n" + 
+				"			\"ZWPZFL_JZFX\": \"1\",\r\n" + 
+				"			\"ZWPZFL_KMBH\": \"2241.98.10\",\r\n" + 
+				"			\"ZWFZYS_BMBH\": \"00\",\r\n" + 
+				"			\"ZWFZYS_WLDWBH\": \"00\",\r\n" + 
+				"			\"ZWFZYS_ZGBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_CPBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH1\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH2\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH3\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH4\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH5\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH6\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH7\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH8\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH9\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH10\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH11\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH12\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH13\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH14\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH15\": \"\",\r\n" + 
+				"			\"ZWFZYS_WBBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_SL\": 0,\r\n" + 
+				"			\"ZWFZYS_DJ\": 0,\r\n" + 
+				"			\"ZWFZYS_WB\": 0,\r\n" + 
+				"			\"ZWFZYS_HL\": 1,\r\n" + 
+				"			\"ZWFZYS_JE\": 2,\r\n" + 
+				"			\"ZWFZYS_YWRQ\": \"\",\r\n" + 
+				"			\"ZWFZYS_PJH\": \"\",\r\n" + 
+				"			\"ZWFZYS_YT\": \"\",\r\n" + 
+				"			\"ZWFZYS_YSBH\":\"\"\r\n" + 
+				"		},\r\n" + 
+				"		{\r\n" + 
+				"			\"ZWFZYS_ID\": \"\",\r\n" + 
+				"			\"ZWPZFL_ZY\": \"付史秀琴应付挂账款\",\r\n" + 
+				"			\"ZWPZFL_JZFX\": \"2\",\r\n" + 
+				"			\"ZWPZFL_KMBH\": \"1001.01\",\r\n" + 
+				"			\"ZWFZYS_BMBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_WLDWBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_ZGBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_CPBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH1\": \"CI1.02.04.01\",\r\n" + 
+				"			\"ZWFZYS_XMBH2\": \"0\",\r\n" + 
+				"			\"ZWFZYS_XMBH3\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH4\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH5\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH6\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH7\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH8\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH9\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH10\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH11\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH12\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH13\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH14\": \"\",\r\n" + 
+				"			\"ZWFZYS_XMBH15\": \"\",\r\n" + 
+				"			\"ZWFZYS_WBBH\": \"\",\r\n" + 
+				"			\"ZWFZYS_SL\": 0,\r\n" + 
+				"			\"ZWFZYS_DJ\": 0,\r\n" + 
+				"			\"ZWFZYS_WB\": 0,\r\n" + 
+				"			\"ZWFZYS_HL\": 1,\r\n" + 
+				"			\"ZWFZYS_JE\": 2,\r\n" + 
+				"			\"ZWFZYS_YWRQ\": \"\",\r\n" + 
+				"			\"ZWFZYS_PJH\": \"\",\r\n" + 
+				"			\"ZWFZYS_YT\": \"\",\r\n" + 
+				"			\"ZWFZYS_YSBH\":\"\"\r\n" + 
+				"		}\r\n" + 
+				"	]\r\n" + 
+				"}";
+		JaxWsDynamicClientFactory clientFactory =JaxWsDynamicClientFactory.newInstance();
+		Client client = clientFactory.createClient("http://127.0.0.1:8080/sqgx/ws/voucher?wsdl");
+		if(client!=null) {
+			LOGGER.info("链接成功");
+			try {
+				Object[] result = client.invoke("getVoucherData", jsonData);
+				LOGGER.info(result[0].toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	public void test() {
+		TestVoucherService tvs = new TestVoucherService();
+		tvs.transVoucher();
+	}
+}
